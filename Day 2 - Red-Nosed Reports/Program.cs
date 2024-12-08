b7821c9bab0c20634f1a2bd40e90e99eb2084800
Jesper Højlund <github@hillgrove.dk>
2024-12-08 23:39:23 +0100

    feat: solving day 2

---
+++ Day 2 - Red-Nosed Reports/Program.cs
@@ -0,0 +1,39 @@
+﻿
+string input = """
+7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9
+""";
+
+int MIN_DIFFERENCE = 1;
+int MAX_DIFFERENCE = 3;
+
+var rows = input.Split("\r\n");
+var data = rows.Select(row => row.Trim()
+                                 .Split(" ")
+                                 .Select(int.Parse)
+                                 .ToList())
+               .ToList();
+
+foreach (var row in data)
+{
+    Console.WriteLine($"[{String.Join(", ", row)}]");
+}
+
+int safeReports = 0;
+foreach (var row in data)
+{
+    for (int i = 0; i < row.Count - 1; i++)
+    {
+        int difference = Math.Abs(row[i] - row[i + 1]);
+        if (MIN_DIFFERENCE < difference || difference > MAX_DIFFERENCE)
+        {
+            break;
+        }
+
+
+    }
+}
